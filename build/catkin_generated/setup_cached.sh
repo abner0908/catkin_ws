59c13c55443ebcb9d148df342da50d71f94cdf72
abner0908 <abner0908@gmail.com>
2015-11-24 09:35:00 +0800

    add talker and listener script

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,4 +7,6 @@
 # new environment variables
 
 # modified environment variables
+export LD_LIBRARY_PATH="/home/abner0908/catkin_ws/devel/lib/x86_64-linux-gnu:/opt/ros/jade/lib/x86_64-linux-gnu:/home/abner0908/catkin_ws/devel/lib:/opt/ros/jade/lib"
+export PKG_CONFIG_PATH="/home/abner0908/catkin_ws/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/jade/lib/x86_64-linux-gnu/pkgconfig:/home/abner0908/catkin_ws/devel/lib/pkgconfig:/opt/ros/jade/lib/pkgconfig"
 export PWD="/home/abner0908/catkin_ws/build"
